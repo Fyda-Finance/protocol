8f8301dcba320e9a475996332335d337ebd70df6
narayanprusty <arsenal.narayan@gmail.com>
2023-12-04 15:51:29 +0530

    debug error

--- deploy/2-test-mock-deploy.ts
+++ deploy/2-test-mock-deploy.ts
@@ -3,7 +3,7 @@ import hre from "hardhat";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 
 import deployDiamond from "../scripts/deploy";
-import { BuyFacet, PriceOracleFacet, ScenarioDEX, StrategyFacet } from "../typechain-types";
+import { BuyFacet, PriceOracleFacet, ScenarioDEX, SellFacet, StrategyFacet } from "../typechain-types";
 
 const addresses: any = {
   goerli: {
@@ -11,7 +11,7 @@ const addresses: any = {
     wbtc: "0xA39434A63A52E749F02807ae27335515BA4b07F7",
     eth: "0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e",
     link: "0x48731cF7e84dc94C5f84577882c14Be11a5B7456",
-    diamond: "0xD9105CaABbb953759aF7f3EeA0Ae9340D8a4B626",
+    diamond: "0x8E215C804cf2a77D1230c1A9f5faF7CdbEeb10C8",
   },
 };
 
@@ -27,92 +27,23 @@ module.exports = async ({ network, getNamedAccounts, deployments }: HardhatRunti
    * 3. Configure Price for ERC20 in DEX and Diamond
    */
 
-  const usdc = await hre.ethers.getContract("USDC");
-  const wbtc = await hre.ethers.getContract("WBTC");
-  const weth = await hre.ethers.getContract("WETH");
-  const link = await hre.ethers.getContract("LINK");
-
-  const dex: ScenarioDEX = await hre.ethers.getContract("MockDEX");
-
-  // const priceOracleFacet: PriceOracleFacet = await ethers.getContractAt(
-  //   "PriceOracleFacet",
-  //   addresses[network.name].diamond
-  // );
-  // Mint USDC
-  const recipientAddress = "0x7295F623794dcEE49d0B3490fF98B40B83a184Ad"; // Replace with the recipient's address
-  const amountToMint = ethers.utils.parseUnits("1000", 6); // Replace with the desired amount
-
-  // try {
-  //   // Mint USDC
-  //   const usdcMintTx = await usdc
-  //     .connect(accounts[0])
-  //     .mint(recipientAddress, amountToMint);
-  //   await usdcMintTx.wait();
-  //   console.log(
-  //     `Minted ${ethers.utils.formatUnits(
-  //       amountToMint,
-  //       6
-  //     )} USDC to ${recipientAddress}`
-  //   );
-  // } catch (error) {
-  //   console.error("Error minting USDC:", error);
-  // }
-
-  const budget = "1000000000";
-  // const budgetApproval = await usdc
-  //   .connect(accounts[1])
-  //   .approve(addresses[network.name].diamond, budget);
-  // await budgetApproval.wait();
   console.log("Fetching strategy Facets");
   const strategyFacet: StrategyFacet = await ethers.getContractAt("StrategyFacet", addresses[network.name].diamond);
-
-  const parameters = {
-    _investToken: weth.address,
-    _stableToken: usdc.address,
-    _stableAmount: "100000000",
-    _investAmount: "0",
-    _impact: 10,
-    _floorType: 0,
-    _floorValue: "0",
-    _liquidateOnFloor: false,
-    _cancelOnFloor: false,
-    _buyType: 1,
-    _buyValue: "1900000000",
-    _buyTwapTime: 0,
-    _buyTwapTimeUnit: 0,
-    _btdValue: "0",
-    _btdType: 0,
-    _buyDCAUnit: 0,
-    _buyDCAValue: "0",
-    _sellType: 0,
-    _sellValue: "0",
-    _highSellValue: "0",
-    _strValue: "0",
-    _strType: 0,
-    _sellDCAUnit: 0,
-    _sellDCAValue: "0",
-    _sellTwapTime: 0,
-    _sellTwapTimeUnit: 0,
-    _completeOnSell: false,
-    _current_price: 0,
-  };
-  const createStrategy = await strategyFacet.connect(accounts[1]).createStrategy(parameters);
-  await createStrategy.wait();
-  const strategy = await strategyFacet.getStrategy(1);
-  console.log("Strategy: ", strategy);
-  // let value = await buyFacet.executionBuyAmount(true, 1);
-  // console.log("Value: ", value);
-  // let dexCalldata = dex.interface.encodeFunctionData("swap", [
-  //   usdc.address,
-  //   weth.address,
-  //   value,
-  // ]);
-  // console.log("Dex call Data ", dexCalldata);
-  // console.log("Dex address: ", dex.address);
-  // const executeBuy = await buyFacet
-  //   .connect(accounts[1])
-  //   .executeBuy(1, { dex: dex.address, callData: dexCalldata });
-  // await executeBuy.wait();
+  const sellFacet: SellFacet = await ethers.getContractAt("SellFacet", addresses[network.name].diamond, accounts[1]);
+  console.log((await sellFacet.executionSellAmount(true, 7)).toString());
+  const executeSell = await sellFacet.callStatic.executeSTR(
+    7,
+    "18446744073709552204",
+    "18446744073709570418",
+    "18446744073709552204",
+    "18446744073709570419",
+    {
+      dex: "0x101E628cbC91c6b0c6348bde885a125C29A9229E",
+      callData:
+        "0xdf791e500000000000000000000000003e6ffe1dd604c3315ce48eb9cf1121a3062768d50000000000000000000000008fd6903611c717bc8673dd890ec5902551c15d8200000000000000000000000000000000000000000000000000000000000f4240",
+    },
+  );
+  await executeSell.wait();
 
   // console.log("Succefully connected with contracts");
 };
